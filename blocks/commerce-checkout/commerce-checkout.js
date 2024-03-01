e949586be9b40998e62c57ee02c3acdb57c3b033
Jurica Migač <jurica1.migac@gmail.com>
2024-03-01 12:59:50 +0100

    PLP Enablement (#3)
    * PLP Enablement
    
    * Fixing lint issues with build
    
    * Fixing Product List add to cart
    
    * Fixing Lint errors
    
    ---------
    
    Co-authored-by: Jurica Migac <jurica.migac@ibm.com>

--- blocks/commerce-checkout/commerce-checkout.js
+++ blocks/commerce-checkout/commerce-checkout.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { readBlockConfig } from '../../scripts/aem.js';
-
-export default function decorate(block) {
-  const config = readBlockConfig(block);
-
-  const content = document.createRange().createContextualFragment(`<div>
-    Commerce Checkout drop-in
-    <pre>${JSON.stringify(config, null, 2)}</pre>
-  </div>`);
-
-  block.textContent = '';
-  block.append(content);
-=======
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/no-extraneous-dependencies */
 
@@ -63,5 +49,4 @@ export default async function decorate(block) {
       },
     },
   })(block);
->>>>>>> boilerplate-plugins/main
 }
--- blocks/header/header.js
+++ blocks/header/header.js
@@ -19,9 +19,7 @@ function closeOnEscape(e) {
   if (e.code === 'Escape') {
     const nav = document.getElementById('nav');
     const navSections = nav.querySelector('.nav-sections');
-    const navSectionExpanded = navSections.querySelector(
-      '[aria-expanded="true"]'
-    );
+    const navSectionExpanded = navSections.querySelector('[aria-expanded="true"]');
     if (navSectionExpanded && isDesktop.matches) {
       // eslint-disable-next-line no-use-before-define
       toggleAllNavSections(navSections);
@@ -73,14 +71,8 @@ function toggleMenu(nav, navSections, forceExpanded = null) {
   const button = nav.querySelector('.nav-hamburger button');
   document.body.style.overflowY = expanded || isDesktop.matches ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(
-    navSections,
-    expanded || isDesktop.matches ? 'false' : 'true'
-  );
-  button.setAttribute(
-    'aria-label',
-    expanded ? 'Open navigation' : 'Close navigation'
-  );
+  toggleAllNavSections(navSections, expanded || isDesktop.matches ? 'false' : 'true');
+  button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
   // enable nav dropdown keyboard accessibility
   const navDrops = navSections.querySelectorAll('.nav-drop');
   if (isDesktop.matches) {
@@ -140,14 +132,14 @@ export default async function decorate(block) {
     navSections
       .querySelectorAll(':scope .default-content-wrapper > ul > li')
       .forEach((navSection) => {
-        if (navSection.querySelector('ul'))
+        if (navSection.querySelector('ul')) {
           navSection.classList.add('nav-drop');
+        }
         navSection.addEventListener('click', () => {
           if (isDesktop.matches) {
             const expanded = navSection.getAttribute('aria-expanded') === 'true';
             toggleAllNavSections(navSections);
-            navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true'
-            );
+            navSection.setAttribute('aria-expanded', expanded ? 'false' : 'true');
           }
         });
       });
@@ -171,14 +163,12 @@ export default async function decorate(block) {
   cartButton.setAttribute('aria-label', 'Cart');
 
   async function toggleMiniCart(state) {
-    const show =
-      state ?? !minicartPanel.classList.contains('nav-tools-panel--show');
+    const show = state ?? !minicartPanel.classList.contains('nav-tools-panel-show');
 
     if (show) {
       await cartProvider.render(MiniCart, {
         routeEmptyCartCTA: () => '/',
-        routeProduct: (product) =>
-          `/products/${product.url.urlKey}/${product.sku}`,
+        routeProduct: (product) => `/products/${product.url.urlKey}/${product.sku}`,
         routeCart: () => '/cart',
         routeCheckout: () => '/checkout',
       })(minicartPanel);
@@ -186,7 +176,7 @@ export default async function decorate(block) {
       minicartPanel.innerHTML = '';
     }
 
-    minicartPanel.classList.toggle('nav-tools-panel--show', show);
+    minicartPanel.classList.toggle('nav-tools-panel-show', show);
   }
 
   cartButton.addEventListener('click', () => toggleMiniCart());
@@ -201,7 +191,7 @@ export default async function decorate(block) {
         cartButton.removeAttribute('data-count');
       }
     },
-    { eager: true }
+    { eager: true },
   );
 
   /** Search */
@@ -222,10 +212,9 @@ export default async function decorate(block) {
   const searchInput = searchPanel.querySelector('input');
 
   function toggleSearch(state) {
-    const show =
-      state ?? !searchPanel.classList.contains('nav-tools-panel--show');
+    const show = state ?? !searchPanel.classList.contains('nav-tools-panel-show');
 
-    searchPanel.classList.toggle('nav-tools-panel--show', show);
+    searchPanel.classList.toggle('nav-tools-panel-show', show);
 
     if (show) searchInput.focus();
   }
@@ -256,10 +245,7 @@ export default async function decorate(block) {
   nav.setAttribute('aria-expanded', 'false');
   // prevent mobile nav behavior on window resize
   toggleMenu(nav, navSections, isDesktop.matches);
-  isDesktop.addEventListener('change', () =>
-    toggleMenu(nav, navSections, isDesktop.matches)
-  );
-
+  isDesktop.addEventListener('change', () => toggleMenu(nav, navSections, isDesktop.matches));
   const navWrapper = document.createElement('div');
   navWrapper.className = 'nav-wrapper';
   navWrapper.append(nav);
